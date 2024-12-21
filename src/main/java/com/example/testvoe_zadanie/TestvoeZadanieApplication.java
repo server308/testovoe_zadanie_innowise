2e6e63b536fc6c440ff4edb9d41e2a1de15cb2c6
server308 <sinijvitalik@gmail.com>
2024-12-21 13:27:43 +0300

    creating a new project

---
+++ src/main/java/com/example/testvoe_zadanie/TestvoeZadanieApplication.java
@@ -0,0 +1,13 @@
+package com.example.testvoe_zadanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestvoeZadanieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestvoeZadanieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testvoe_zadanie/TestvoeZadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testvoe_zadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestvoeZadanieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
